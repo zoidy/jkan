f0878569976ffeded7335e9ab8dd645b929ff99b
Fernando Rios <zoidy@users.noreply.github.com>
2025-01-29 17:52:19 -0500

    again

--- scripts/src/templates/dataset-item.js
+++ scripts/src/templates/dataset-item.js
@@ -1,6 +1,6 @@
 export default (data) => (
 `<dataset>
   <h3><a href="${data.url}">${data.title}</a></h3>
-  ${data.notes || '' | truncate: 300}
+  ${data.notes.length > 300 ? data.notes.substring(0,300) + '...' : data.notes || ''}
 </dataset>`
 )
